b7c8f0bbe1076a4f11224925152288c285989437
Lukas Köppl <traktor.koeppl@gmail.com>
2025-04-03 19:10:42 +0200

    add picture to project and load sucessful :)

--- src/util/bmp_reader.c
+++ src/util/bmp_reader.c
@@ -1,13 +1,77 @@
-//Test
 #include <ctoy.h>
+#include <stdio.h> // optional (stdio.h is already included in ctoy.h)
 
-void ctoy_begin()
-{
-   printf("Hello, World!\n");
+// debugging!!!
+#define MEMPTR_READ_TEST 1
+
+struct m_image framebuffer = M_IMAGE_IDENTITY(); // initialize the struct (all set to zero in this case)
+
+typedef struct {
+   uint8_t *ptr;
+   size_t size;
+} MEMPTR;
+
+MEMPTR readFileToMemory(const char *filename) {
+   FILE *fp = fopen(filename, "rb");
+
+   MEMPTR mtr = {NULL, 0};
+
+   if (!fp) {
+       perror("File opening failed");
+       return mtr; 
+   }
+
+   if (fseek(fp, 0L, SEEK_END) != 0) {
+       perror("File seek failed");
+       fclose(fp);
+       return mtr;
+   }
+   
+   size_t size = ftell(fp);
+   rewind(fp);
+   
+   uint8_t *ptr = malloc(size);
+   if (!ptr) {
+       perror("Memory allocation failed");
+       fclose(fp);
+       return mtr;
+   }
+   
+   if (fread(ptr, sizeof(uint8_t), size, fp) != size) {
+       perror("File read failed");
+       free(ptr);
+       fclose(fp);
+       return mtr;
+   }
+   
+   fclose(fp);
+
+   mtr.ptr = ptr;
+   mtr.size = size;
+   #if MEMPTR_READ_TEST == 1
+   printf("Pointer Address: %p\n", (void*)mtr.ptr); // Zeigeradresse ausgeben
+   printf("Size: %zu\n", mtr.size);
+   #endif
+   return mtr;
 }
 
-void ctoy_main_loop()
+
+
+void ctoy_begin(void)
+{
+   printf("\nHello World!\n");
+   ctoy_window_title("Hello-World!");
+
+   MEMPTR mem = readFileToMemory("data/tetris.bmp");
+   if (mem.ptr == NULL) {
+      printf("data/tetris.bmp not found\n");
+      return;
+   }
+   
+}
+
+void ctoy_end(void)
 {}
 
-void ctoy_end()
+void ctoy_main_loop(void)
 {}
